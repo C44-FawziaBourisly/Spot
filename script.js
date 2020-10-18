8241cdfdbb54f3d8d0957cfbe0caa6f0a1ef6be4
mbourisly <49692793+mbourisly@users.noreply.github.com>
2020-10-18 19:45:50 +0300

    counter animation

---
+++ script.js
@@ -0,0 +1,33 @@
+new Glider(document.querySelector(".glider"), {
+  slidesToShow: 3,
+  slidesToScroll: 5,
+  draggable: true,
+  dots: ".dots",
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+});
+
+// ////////////animated counter//////////////////
+
+const counters = document.querySelectorAll(".counter");
+const speed = 200;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+
+  updateCount();
+});
