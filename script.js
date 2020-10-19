c19a78d18ec8c95369f60c0bc46274701c031a12
mbourisly <49692793+mbourisly@users.noreply.github.com>
2020-10-19 22:25:55 +0300

    sign in

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ counters.forEach((counter) => {
 
   updateCount();
 });
+
+// //////////////////////sign in firebase///////////////////////////
+
+firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
