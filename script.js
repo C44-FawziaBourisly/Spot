6f33e94a0439b2d7565053d7d7479c89c043ff03
mbourisly <49692793+mbourisly@users.noreply.github.com>
2020-10-24 21:39:10 +0300

    .

--- script.js
+++ script.js
@@ -221,20 +221,6 @@ $(".navigate-section > li > a[href*=#], a.navigate-btn[href*=#]").on(
 //   });
 // });
 
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("logo").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("navbar").style.background = "#3b4465";
-//     document.getElementById("logo").style.fontSize = "35px";
-//   }
-// }
 // scrollFunction();
 
 // var submitContact = $("#submit_contact"),
